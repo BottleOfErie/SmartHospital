7fff36d6496bd028d4049b1057add9185f0c2b85
YICHEN ZHONG <1120222298@bit.edu.cn>
2024-08-24 10:20:42 +0800

    Update sqliteOperator.cpp

--- cpp/sqliteOperator.cpp
+++ cpp/sqliteOperator.cpp
@@ -8,29 +8,31 @@
 #include <QSqlTableModel>
 
 SqliteOperator::SqliteOperator() {
-    this->createTable();
+    this->connect("General.db");
+    createAccountTable();
 }
 
 bool SqliteOperator::connect(const QString &dbName) {
-    this->accountDb = QSqlDatabase::addDatabase("QSQLITE", QString("%1").arg(dbName));
-    this->accountDb.setDatabaseName(dbName);
-    if (!this->accountDb.open()) {
-        QMessageBox::critical(0, QObject::tr("Database Error"), this->accountDb.lastError().text());
+    this->Db = QSqlDatabase::addDatabase("QSQLITE", QString("%1").arg(dbName));
+    this->Db.setDatabaseName(dbName);
+    if (!this->Db.open()) {
+        QMessageBox::critical(0, QObject::tr("Database Error"), this->Db.lastError().text());
         return false;
     }
     return true;
 }
 
 
-bool SqliteOperator::createTable()
+bool SqliteOperator::createAccountTable()
 {
-    if (connect("Account.db")) {
 //        qDebug() << this->accountDb.open();
         // 用于执行sql语句的对象
-        QSqlQuery query(this->accountDb);
-        if (!query.exec("CREATE TABLE Account("
-        "userName TEXT PRIMARY KEY,"
-        "password CHARACTER(20) )")) {
+        QSqlQuery query(this->Db);
+        query.prepare("CREATE TABLE Account("
+                      "userName TEXT PRIMARY KEY,"
+                      "password CHARACTER(20) )");
+
+        if (!query.exec()) {
 //            QMessageBox::critical(0,
 //            QObject::tr("Database Error"),
 //            query.lastError().text());
@@ -39,17 +41,18 @@ bool SqliteOperator::createTable()
             qDebug() << "Table created!";
             return true;
         }
-    }
-    accountDb.close();
+
+    Db.close();
     return true;
 }
 
-//I think QVariant should be ok. But it needed to be tested
-bool SqliteOperator::singleInsertData(QVariant userName, QVariant Password) {
-    accountDb.open();
-    QSqlQuery query2(this->accountDb);
+//I think QString should be ok. But it needed to be tested
+bool SqliteOperator::Login(QString userName, QString Password, QString tableName) {
+    Db.open();
+    QSqlQuery query2(this->Db);
     query2.prepare("SELECT password FROM Account WHERE username = :username");
     query2.bindValue(":username", userName);
+
     if (!query2.exec()) {
         qDebug() << "Query failed:" << query2.lastError();
         return false;
@@ -58,27 +61,18 @@ bool SqliteOperator::singleInsertData(QVariant userName, QVariant Password) {
         return false;
     }
 
-    QSqlQuery query(this->accountDb);
-    query.prepare("INSERT INTO Account VALUES(?, ?)");
-    QVariantList username;
-    username << userName;
-    query.addBindValue(username);
-    QVariantList password;
-    password << Password;
-    query.addBindValue(password);
-
-    if (!query.execBatch()) {
-        QMessageBox::critical(0, QObject::tr("Database Error"),
-        query.lastError().text());
-    }
-
-    accountDb.close();
+    QSqlQuery query(this->Db);
+    query.prepare("INSERT INTO Account VALUES(:username, :password)");
+    query.bindValue(":username", userName);
+    query.bindValue(":password", Password);
+    query.exec();
+    Db.close();
     return true;
 }
 
 bool SqliteOperator::validateLogin(QString username, QString InputPassword) {
-    accountDb.open();
-    QSqlQuery query(this->accountDb);
+    Db.open();
+    QSqlQuery query(this->Db);
     query.prepare("SELECT password FROM Account WHERE username = :username");
     query.bindValue(":username", username);
     if (!query.exec()) {
@@ -88,7 +82,6 @@ bool SqliteOperator::validateLogin(QString username, QString InputPassword) {
 
     if (query.next()) {
         QString storedPassword = query.value(0).toString();
-
         if (storedPassword == InputPassword) {
             return true;
         } else {
@@ -98,6 +91,6 @@ bool SqliteOperator::validateLogin(QString username, QString InputPassword) {
         qDebug() << "No Such user found";
         return false;
     }
-    accountDb.close();
+   Db.close();
 }
 
