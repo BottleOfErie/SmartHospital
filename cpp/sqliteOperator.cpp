da7f17775ff7270a5ed65ca3f88bb5703da9624d
YICHEN ZHONG <1120222298@bit.edu.cn>
2024-08-24 10:56:08 +0800

    I've finished createTable function(used to create all tables needed), the Login and validateLogin function used to signing up and signing in
    delete, change, insert function of the rest tables still need to be finished

--- cpp/sqliteOperator.cpp
+++ cpp/sqliteOperator.cpp
@@ -9,7 +9,7 @@
 
 SqliteOperator::SqliteOperator() {
     this->connect("General.db");
-    createAccountTable();
+    createTables();
 }
 
 bool SqliteOperator::connect(const QString &dbName) {
@@ -23,31 +23,133 @@ bool SqliteOperator::connect(const QString &dbName) {
 }
 
 
-bool SqliteOperator::createAccountTable()
+bool SqliteOperator::createTables()
 {
-//        qDebug() << this->accountDb.open();
-        // 用于执行sql语句的对象
-        QSqlQuery query(this->Db);
-        query.prepare("CREATE TABLE Account("
-                      "userName TEXT PRIMARY KEY,"
-                      "password CHARACTER(20) )");
+    // 打开数据库
+    if (!this->Db.open()) {
+        qDebug() << "Database open error!";
+        return false;
+    }
 
-        if (!query.exec()) {
-//            QMessageBox::critical(0,
-//            QObject::tr("Database Error"),
-//            query.lastError().text());
-            return false;
-        } else {
-            qDebug() << "Table created!";
-            return true;
-        }
+    QSqlQuery query(this->Db);
 
+    // 创建 Account 表
+    query.prepare("CREATE TABLE Account("
+                  "userName TEXT PRIMARY KEY,"
+                  "password CHARACTER(20))");
+    if (!query.exec()) {
+        qDebug() << "Error creating Account table: " << query.lastError().text();
+        return false;
+    } else {
+        qDebug() << "Account table created!";
+    }
+
+    // 创建病人表
+    query.prepare("CREATE TABLE Patient("
+                  "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                  "name TEXT NOT NULL,"
+                  "idCard TEXT UNIQUE NOT NULL,"
+                  "username TEXT UNIQUE NOT NULL,"
+                  "password TEXT NOT NULL,"
+                  "gender TEXT,"
+                  "birthdate DATE,"
+                  "phone TEXT)");
+    if (!query.exec()) {
+        qDebug() << "Error creating Patient table: " << query.lastError().text();
+        return false;
+    }
+
+    // 创建医生表
+    query.prepare("CREATE TABLE Doctor("
+                  "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                  "name TEXT NOT NULL,"
+                  "idCard TEXT UNIQUE NOT NULL,"
+                  "username TEXT UNIQUE NOT NULL,"
+                  "password TEXT NOT NULL,"
+                  "gender TEXT,"
+                  "birthdate DATE,"
+                  "phone TEXT,"
+                  "title TEXT,"
+                  "hospital TEXT,"
+                  "department TEXT)");
+    if (!query.exec()) {
+        qDebug() << "Error creating Doctor table: " << query.lastError().text();
+        return false;
+    }
+
+    // 创建病历表
+    query.prepare("CREATE TABLE MedicalRecord("
+                  "patientId INTEGER,"
+                  "doctorId INTEGER,"
+                  "dateTime DATETIME,"
+                  "chatRecord TEXT,"
+                  "diagnosisDetails TEXT,"
+                  "medicalAdvice TEXT,"
+                  "PRIMARY KEY (patientId, doctorId, dateTime),"
+                  "FOREIGN KEY (patientId) REFERENCES Patient(id),"
+                  "FOREIGN KEY (doctorId) REFERENCES Doctor(id))");
+    if (!query.exec()) {
+        qDebug() << "Error creating MedicalRecord table: " << query.lastError().text();
+        return false;
+    }
+
+    // 创建处方表
+    query.prepare("CREATE TABLE Prescription("
+                  "patientId INTEGER,"
+                  "doctorId INTEGER,"
+                  "dateTime DATETIME,"
+                  "medicineName TEXT,"
+                  "medicinePrice REAL,"
+                  "medicineQuantity INTEGER,"
+                  "usageAdvice TEXT,"
+                  "PRIMARY KEY (patientId, doctorId, dateTime, medicineName),"
+                  "FOREIGN KEY (patientId) REFERENCES Patient(id),"
+                  "FOREIGN KEY (doctorId) REFERENCES Doctor(id))");
+    if (!query.exec()) {
+        qDebug() << "Error creating Prescription table: " << query.lastError().text();
+        return false;
+    }
+
+    // 创建预约信息表
+    query.prepare("CREATE TABLE Appointment("
+                  "patientId INTEGER,"
+                  "doctorId INTEGER,"
+                  "dateTime DATETIME,"
+                  "isCompleted BOOLEAN,"
+                  "PRIMARY KEY (patientId, doctorId, dateTime),"
+                  "FOREIGN KEY (patientId) REFERENCES Patient(id),"
+                  "FOREIGN KEY (doctorId) REFERENCES Doctor(id))");
+    if (!query.exec()) {
+        qDebug() << "Error creating Appointment table: " << query.lastError().text();
+        return false;
+    }
+
+    // 创建健康评估信息表
+    query.prepare("CREATE TABLE HealthAssessment("
+                  "patientId INTEGER,"
+                  "dateTime DATETIME,"
+                  "height REAL,"
+                  "weight REAL,"
+                  "heartRate INTEGER,"
+                  "bloodPressureHigh INTEGER,"
+                  "bloodPressureLow INTEGER,"
+                  "lungCapacity REAL,"
+                  "PRIMARY KEY (patientId, dateTime),"
+                  "FOREIGN KEY (patientId) REFERENCES Patient(id))");
+    if (!query.exec()) {
+        qDebug() << "Error creating HealthAssessment table: " << query.lastError().text();
+        return false;
+    }
+
+    qDebug() << "All tables created successfully!";
+
+    // 关闭数据库
     Db.close();
     return true;
 }
 
 //I think QString should be ok. But it needed to be tested
-bool SqliteOperator::Login(QString userName, QString Password, QString tableName) {
+bool SqliteOperator::Login(QString userName, QString Password) {
     Db.open();
     QSqlQuery query2(this->Db);
     query2.prepare("SELECT password FROM Account WHERE username = :username");
@@ -93,4 +195,5 @@ bool SqliteOperator::validateLogin(QString username, QString InputPassword) {
     }
    Db.close();
 }
+
 
