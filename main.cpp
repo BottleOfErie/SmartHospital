6125a17ac9384d42393e2eecf9201370a8c17f19
BottleOfErie <Bottle_of_Erie@outlook.com>
2024-08-27 22:49:31 +0800

    add ai support

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <h/mainwindow.h>
 
+#include "net/QwenClient.h"
 #include "net/ServerThread.h"
 #include "net/ClientSocket.h"
 
@@ -8,16 +9,34 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
 
-    qDebug("Started\n");
+    bool debug = true;
 
-    auto server = new ServerThread(34569);
-    ClientSocket::getInstance().connectToIP("127.0.0.1",34569);
-
+    if(!debug){
+        bool isClient=true;
+        for(int i=0;i<argc;i++){
+            auto arg=QString(argv[i]);
+            if(arg.compare("-ai")==0){
+                QwenClient::getInstance().setAPI_KEY(argv[i+1]);
+                qDebug("Taken Ai ApiKey:%s",argv[i+1]);
+                i++;
+            }else if(arg.compare("-server")==0){
+                isClient=false;
+                auto server = new ServerThread(34569);
+            }
+        }
+        if(isClient){
+            ClientSocket::getInstance().connectToIP("127.0.0.1",34569);
+            w.show();
+        }
+    }else{
+        auto server = new ServerThread(34569);
+        ClientSocket::getInstance().connectToIP("127.0.0.1",34569);
+        w.show();
+    }
     /*ClientSocket::getInstance().submitAppointment((NetUtils::Appointment){
-1,2,"2004-04-06",0
-                                                  });*/
+1,2,"2004-04-06",0});*/
+    QwenClient::getInstance().ask("我有点腰疼，弯腰的时候腰很酸。我现在正在医院门诊挂号，请你简短地告诉我应该挂什么科室？");
 
     return a.exec();
 
---
+++ net/QwenClient.cpp
@@ -0,0 +1,87 @@
+#include <QJsonParseError>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QUrlQuery>
+#include "QwenClient.h"
+
+QwenClient& QwenClient::getInstance(){
+    static QwenClient instance;
+    return instance;
+}
+
+QwenClient::QwenClient()
+{
+    manager=new QNetworkAccessManager(this);
+
+    connect(manager,SIGNAL(finished(QNetworkReply*)),this,SLOT(networkReply(QNetworkReply*)));
+}
+
+QwenClient::~QwenClient(){
+    delete manager;
+}
+
+void QwenClient::setAPI_KEY(char* key){
+    API_KEY=QByteArray(key);
+}
+
+void QwenClient::networkReply(QNetworkReply* reply){
+    QString info = reply->readAll();
+    QJsonParseError err;
+    QJsonDocument doc=QJsonDocument::fromJson(info.toUtf8(),&err);
+    bool hasReply=false;
+    if(!doc.isNull()){
+        QJsonObject file=doc.object();
+        if(file.contains("output")){
+            QJsonValue output=file.value("output");
+            if(output.isObject()){
+                QJsonObject outputObj=output.toObject();
+                if(outputObj.contains("choices")){
+                    QJsonValue choices=outputObj.value("choices");
+                    if(choices.isArray()){
+                        QJsonArray choicesObj=choices.toArray();
+                        if(choicesObj.count()>0){
+                            QJsonValue first=choicesObj[0];
+                            if(first.isObject()){
+                                QJsonObject firstObj=first.toObject();
+                                if(firstObj.contains("message")){
+                                    QJsonValue message=firstObj.value("message");
+                                    if(message.isObject()){
+                                        QJsonObject messageObj=message.toObject();
+                                        if(messageObj.contains("content")){
+                                            QJsonValue content=messageObj.value("content");
+                                            if(content.isString()){
+                                                QString str=content.toString();
+                                                hasReply=true;
+                                                qDebug("aiReply:%s",str.toStdString().data());
+                                                emit aiReply(str);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    reply->deleteLater();
+}
+
+void QwenClient::ask(QString question){
+    if(API_KEY.compare("")==0){
+        qDebug("Cannot Use Ai Service!No API_KEY!");
+        emit aiReply("Cannot Use Ai Service!No API_KEY!");
+        return;
+    }
+    QNetworkRequest request=QNetworkRequest(QUrl("https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation"));
+    request.setHeader(QNetworkRequest::ContentTypeHeader,"application/json");
+    request.setRawHeader("Authorization","Bearer "+API_KEY);
+
+    char* prefix="{\"model\": \"qwen-turbo\", \"input\":{\"messages\":[{\"role\": \"system\",\"content\": \"You are a highly advanced medical AI designed to assist patients in adapting to hospital services.\"},{\"role\": \"user\",\"content\":\"";
+    char* suffix="\"}]},\"parameters\": {\"result_format\": \"message\"}}";
+    char requestData[2048];
+    sprintf(requestData,"%s%s%s",prefix,question.toUtf8().data(),suffix);
+    manager->post(request,requestData);
+}
