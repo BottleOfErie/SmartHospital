61f0a1fd1935fdfde306e2f22c5f0705e11514b5
leo8171111 <1508785119@qq.com>
2024-08-27 20:14:56 +0800

    Merge remote-tracking branch 'origin/main' into main

--- h/PatientEditPersonalInformation.h
+++ h/PatientEditPersonalInformation.h
@@ -1,8 +1,7 @@
 #ifndef PATIENTEDITPERSONALINFORMATION_H
 #define PATIENTEDITPERSONALINFORMATION_H
-
 #include <QWidget>
-
+#include "net/NetUtils.h"
 namespace Ui {
 class PatientEditPersonalInformation;
 }
@@ -16,13 +15,40 @@ public:
     void paintEvent(QPaintEvent *e);
     ~PatientEditPersonalInformation();
 
+
 private slots:
+    void on_pushButton_2_clicked();
     void on_pushButton_3_clicked();
 
-    void on_pushButton_2_clicked();
+    void setPatientData_slot(NetUtils::PatientData data);
+    void updatePatientData();
+    void on_pushButton_toggled(bool checked);
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_lineEdit_2_textChanged(const QString &arg1);
+
+    void on_lineEdit_3_textChanged(const QString &arg1);
+
+
+
+    void on_name_textChanged(const QString &arg1);
+
+    void on_age_textChanged(const QString &arg1);
+
+    void on_gender_textChanged(const QString &arg1);
+
+    void on_IDnumber_textChanged(const QString &arg1);
+
+    void on_phone_textChanged(const QString &arg1);
+
+    void on_textEdit_textChanged(const QString &arg1);
+
+    void on_textEdit_2_textChanged(const QString &arg1);
 
 private:
     Ui::PatientEditPersonalInformation *ui;
+    NetUtils::PatientData currentData;
 };
 
-#endif // PATIENTEDITPERSONALINFORMATION_H
+#endif // PatientEDITPERSONALINFORMATION_H
