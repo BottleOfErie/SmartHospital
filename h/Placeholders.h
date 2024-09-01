aa0949d07018c2b9403d179db88ab9cf47f28f95
aurum1918 <2112979621@qq.com>
2024-09-01 17:47:00 +0800

    Update Placeholders.h

--- h/Placeholders.h
+++ h/Placeholders.h
@@ -3,8 +3,9 @@
 
 #define REG_SECTION_PLACEHOLDER "请选择就诊科室"
 #define SECTION_PLACEHOLDER "请选择科室"
+#define SECTION_PLACEHOLDER_DOC "请选择科室（仅医生）"
 #define DOCTOR_PLACEHOLDER "请选择医生"
 #define DATETIME_PLACEHOLDER "请选择就诊时间"
-#define JOBTITLE_PLACEHOLDER "请选择职称"
+#define JOBTITLE_PLACEHOLDER_DOC "请选择职称（仅医生）"
 
 #endif // PLACEHOLDERS_H
